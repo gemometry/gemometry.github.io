049ef2ac03023c8a326a00fe106a191dcc823855
XanderG2 <XanderG2@users.noreply.github.com>
2023-10-03 18:06:21 +0100

    login

---
+++ login.js
@@ -0,0 +1,7 @@
+userpass = prompt("Enter password");
+
+if (userpass == "00789") {
+  location.replace(
+    "https://gemometry.github.io/pidsohgpdfkhgdspofkhedsopfihesfpohrgpohfpijesobfpudhbgfufijdeikrftugjfvkdoeirtyuhjkvfd.html"
+  );
+}
